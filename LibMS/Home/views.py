9e66dd2b50cc4fde4ea3c07a44a0d8346e9ff0d7
Chamika <chamitharukulasekara@gmail.com>
2021-11-10 00:41:03 +0530

    search results

--- LibMS/Home/views.py
+++ LibMS/Home/views.py
@@ -10,16 +10,17 @@ DATA_FORMAT = {
 
 def getBooks(request):
     keyword = request.GET.get('key')
-    
+   
     ### Database
-    books = Books.objects.filter(title='The Hobbit')
-    ###
-    
-    print(books)
+    q="harry"
+    queries = []
+    queries.append( Books.objects.filter(title__icontains=q))
+    queries.append(Books.objects.filter(author__icontains=q))
+    print(queries)
     
     data = DATA_FORMAT
     
     data['id'].append('01')
     data['name'].append('Sample Book')
     
-    return render(request, 'Home/home.html', data)
+    return render (request, 'Home/home.html',data)
