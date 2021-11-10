257369a97fa580bd796e81c07492dc04dfc3d2dd
Suharda Silva <suhardapo@gmail.com>
2021-11-10 11:16:38 +0530

    Search Updated

--- LibMS/Home/views.py
+++ LibMS/Home/views.py
@@ -4,11 +4,11 @@ from Home.models import Books
 # Create your views here.
 def getBooks(request):
     keyword = request.GET.get('key')
-   
+    
     queries = []
     queries.append(Books.objects.filter(title__icontains=keyword))
     queries.append(Books.objects.filter(author__icontains=keyword))
-    queries = [query for query in queries[0] if query]
+    queries = [query for search in queries for query in search if query]
     
     data = {'books': queries}
     
