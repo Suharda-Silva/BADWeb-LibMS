6ff97c50ef2816450ca6388de305d121567c7566
Suharda Silva <suhardapo@gmail.com>
2021-11-11 01:53:07 +0530

    Search Done

--- LibMS/Home/views.py
+++ LibMS/Home/views.py
@@ -9,7 +9,9 @@ def getBooks(request):
     queries.append(Books.objects.filter(title__icontains=keyword))
     queries.append(Books.objects.filter(author__icontains=keyword))
     queries = [query for search in queries for query in search if query]
+    #available = [(book.availability - book.issued) for book in queries]
     
+    #data = {'books': queries, 'available':  available}
     data = {'books': queries}
     
     return render (request, 'Home/home.html', data)
