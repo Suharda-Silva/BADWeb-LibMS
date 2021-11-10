d08857611af137e2b9e5aaee84cb5e38b1d2dad0
Suharda Silva <suhardapo@gmail.com>
2021-11-10 10:55:01 +0530

    Search Query Implimented

--- LibMS/Home/views.py
+++ LibMS/Home/views.py
@@ -4,13 +4,12 @@ from Home.models import Books
 # Create your views here.
 def getBooks(request):
     keyword = request.GET.get('key')
-    
-    print(keyword)
+   
     queries = []
     queries.append(Books.objects.filter(title__icontains=keyword))
     queries.append(Books.objects.filter(author__icontains=keyword))
+    queries = [query for query in queries[0] if query]
     
-    print(queries)
     data = {'books': queries}
     
     return render (request, 'Home/home.html', data)
